b3b1633876d00116b5f46279280567bde6621ea3
Adam Scott <ascott@kusanagi.local>
2010-09-15 11:24:48 +1000

    Adam: Initial commit

---
+++ src/money/Dollar.java
@@ -0,0 +1,9 @@
+package money;
+
+public class Dollar extends Money {
+
+    Dollar(int amount, String currency) {
+        super(amount, currency);
+    }
+    
+}
---
+++ src/money/Franc.java
@@ -0,0 +1,8 @@
+package money;
+
+public class Franc extends Money {
+
+    Franc(int amount, String currency) {
+        super(amount, currency);
+    }
+}
